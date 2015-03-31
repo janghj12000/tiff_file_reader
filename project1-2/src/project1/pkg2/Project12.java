097242593b2a0de8b1505527797d5bbd1676d260
janghj12000 <janghj12000@hotmail.com>
2015-03-30 22:18:10 -0700

    this program read tiff file image
    It can change the image into grayscale and dithering

---
+++ project1-2/src/project1/pkg2/Project12.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project1.pkg2;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author hja23
+ */
+public class Project12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JFrame aa = new pictureframe();
+        aa.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aa.setVisible(true);
+        
+    }
+    
+}
